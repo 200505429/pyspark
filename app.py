a92af5c2922cb3e59fc0563331496660fd3edacf
gtavasant <vasant@coup.digital>
2022-08-13 19:14:24 -0400

    m

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import  Flask, render_template,request
+from backEnd import backEnd
+from flask_cors import CORS
+
+
+
+import json
+app = Flask(__name__)
+CORS(app)
+
+
+
+@app.route('/')
+
+def main():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+
+def home():
+    screenSize = float( request.form['screenSize'])
+    screenRes = int(request.form['ScreenRes'])
+    Cpu = int(request.form['CPU'])
+    RAM = int(request.form['RAM'])
+    weight = float(request.form['Weight'])
+    touchScreen = int(request.form['Touchscreen'])
+    HDD = int(request.form['HDD'])
+    SSD = int(request.form['SSD'])
+    SSHD = int(request.form['SSHD'])
+    FStorage = int(request.form['fStorage'])
+    Type = int(request.form['Type'])
+    Os = int(request.form['Os'])
+
+    back = backEnd(screenSize, screenRes, Cpu, RAM, weight, touchScreen, HDD, SSD, SSHD, FStorage, Type, Os)
+    final_predicted = (round(float(back.totalPredicted[0]),2))
+    
+    return format(final_predicted)
+
+if __name__ == "__main__":
+    app.run(debug=False)
